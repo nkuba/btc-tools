98ef5122e4609321c3d58a1daec49e274164405f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-06-19 09:50:03 +0200

    Create unsigned bitcoin transaction
    Function to create an unsigned bitcoin transaction. It creates a
    transaction with two outputs and one input.

--- pkg/transaction/transaction.go
+++ pkg/transaction/transaction.go
@@ -31,3 +31,58 @@ func ReadTransactionData(filePath string) (*TransactionData, error) {
 
 	return txData, nil
 }
+
+func CreateUnsigned(
+	sourceTxHashString string,
+	sourceTxOutputIndex uint32,
+	sourceTxOutputAmount uint64,
+	outputAddress1 string,
+	outputAddress2 string,
+	outputAmount1 uint64,
+	outputAmount2 uint64,
+	networkParams *chaincfg.Params,
+) (*wire.MsgTx, error) {
+	var msgTx *wire.MsgTx
+
+	addTxOutput := func(address string, amount uint64) error {
+		addr, err := btcutil.DecodeAddress(address, networkParams)
+		if err != nil {
+			return fmt.Errorf("address decoding failed [%s]", err)
+		}
+
+		outScript, err := txscript.PayToAddrScript(addr)
+		if err != nil {
+			return fmt.Errorf("script to pay creation failed [%s]", err)
+		}
+
+		msgTx.AddTxOut(wire.NewTxOut(int64(amount), outScript))
+
+		return nil
+	}
+
+	// Initialize transaction message
+	msgTx = wire.NewMsgTx(wire.TxVersion)
+
+	// Create Transaction input based on the output of historic transaction
+	sourceTxHash, err := chainhash.NewHashFromStr(sourceTxHashString)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create source transaction hash [%s]", err)
+	}
+	txIn := wire.NewTxIn(
+		wire.NewOutPoint(sourceTxHash, sourceTxOutputIndex),
+		nil,
+		nil)
+	msgTx.AddTxIn(txIn)
+
+	// Transaction Output 1.
+	if err := addTxOutput(outputAddress1, outputAmount1); err != nil {
+		return nil, fmt.Errorf("output 1 creation failed [%s]", err)
+	}
+
+	// Transaction Output 2.
+	if err := addTxOutput(outputAddress2, outputAmount2); err != nil {
+		return nil, fmt.Errorf("output 2 creation failed [%s]", err)
+	}
+
+	return msgTx, nil
+}
